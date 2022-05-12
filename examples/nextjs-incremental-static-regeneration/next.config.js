c2f1a583f9ba26d1beb9377c99a3f1e337a84ee2
Thomas Lindstrøm <t@hom.as>
2022-05-12 03:36:37 +0200

    Add WEBSITE_URL into next.config.json

--- examples/nextjs-incremental-static-regeneration/next.config.js
+++ examples/nextjs-incremental-static-regeneration/next.config.js
@@ -1,6 +1,9 @@
 const {locales} = require('tacotranslate');
 
 module.exports = {
+	env: {
+		WEBSITE_URL: 'your-production-website-url'
+	},
 	i18n: {
 		defaultLocale: 'en',
 		locales: locales.map(([code]) => code),
