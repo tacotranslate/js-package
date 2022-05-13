940708846489b45ebc2d81dcd099f9d84cf036fe
Thomas Lindstrøm <t@hom.as>
2022-05-13 19:15:47 +0200

    Update example text

--- examples/nextjs-app/pages/index.jsx
+++ examples/nextjs-app/pages/index.jsx
@@ -15,7 +15,7 @@ const Page = ({outputLocale}) => {
 			</h1>
 
 			<p style={{fontFamily: 'sans-serif', lineHeight: 1.6}}>
-				<Translate string="<span translate='no'>TacoTranslate</span> is a service that automatically translates your <span translate='no'>React</span> application to any language in minutes. Join us in saying <span translate='no'>adiós</span> to JSON files, and <a href='https://tacotranslate.com'>set up your free <span translate='no'>TacoTranslate</span> account</a> today!<br><br>Out of the box, you’ll get to translate one language for free. In addition, we are collaborating with one the top translation providers in the world, should you need to get a more accurate translation than what machine learning can provide.<br><br><a href='https://tacotranslate.com'>Sign up now, <span translate='no'>amigo</span>!</a>" />
+				<Translate string="<span translate='no'>TacoTranslate</span> is a service that automatically translates your React application to any language in minutes. Join us in saying <span translate='no'>adiós</span> to JSON files, and <a href='https://tacotranslate.com'>set up your free <span translate='no'>TacoTranslate</span> account</a> today!<br><br>Out of the box, you’ll get to translate one language for free. In addition, we are collaborating with one the top translation providers in the world, should you need to get a more accurate translation than what machine learning can provide.<br><br><a href='https://tacotranslate.com'>Sign up now, <span translate='no'>amigo</span>!</a>" />
 			</p>
 		</Wrapper>
 	);
