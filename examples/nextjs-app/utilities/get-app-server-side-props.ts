646e2983e3d0d2ae9d1fb16fa68ae7e4abe20fdf
Thomas Lindstrøm <t@hom.as>
2023-01-05 22:40:41 +0100

    Don’t use node: imports

--- examples/nextjs-app/utilities/get-app-server-side-props.ts
+++ examples/nextjs-app/utilities/get-app-server-side-props.ts
@@ -1,4 +1,4 @@
-import process from 'node:process';
+import process from 'process';
 import {type GetServerSidePropsContext} from 'next';
 import tacoTranslate from './tacotranslate';
 
--- examples/nextjs-app/utilities/get-app-static-props.ts
+++ examples/nextjs-app/utilities/get-app-static-props.ts
@@ -1,4 +1,4 @@
-import process from 'node:process';
+import process from 'process';
 import {type GetStaticPropsContext} from 'next';
 import tacoTranslate from './tacotranslate';
 
