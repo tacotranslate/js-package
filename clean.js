86a51a0e4cdb71e398943750892e360b78e1c333
Thomas Lindstrøm <t@hom.as>
2023-04-18 13:07:52 +0200

    Fix formatting

--- clean.js
+++ clean.js
@@ -1,11 +1,11 @@
-const fs = require('fs/promises')
-const config = require('./tsconfig.build.json')
+const fs = require('node:fs/promises');
+const config = require('./tsconfig.build.json');
 
 const promises = [];
 
 for (const file of config.include) {
-	const typeFile = file.replace(/\.tsx?$/, '.d.ts')
-	promises.push(fs.unlink(typeFile))
+	const typeFile = file.replace(/\.tsx?$/, '.d.ts');
+	promises.push(fs.unlink(typeFile));
 }
 
-Promise.all(promises)
+Promise.all(promises);
