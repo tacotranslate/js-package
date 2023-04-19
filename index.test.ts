96dcc9a7fb09a921d497d1a21c07d55570d9e1d1
Thomas Lindstrøm <t@hom.as>
2023-04-19 14:53:32 +0200

    Add some tests for creating entries and retrieving translations

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,26 @@
-import {template} from '.';
+import {
+	createEntry,
+	getEntryFromTranslations,
+	patchDefaultString,
+	template,
+} from '.';
 
 test('template strings should be replaced', () => {
 	expect(template('Hello, {{name}}!', {name: 'Pedro'})).toBe('Hello, Pedro!');
+});
+
+test('default strings should be patched', () => {
+	expect(patchDefaultString('Hello, [[[Pedro]]]!')).toBe('Hello, Pedro!');
+});
+
+test('get entry from translation by string', () => {
+	const translations = {'Hello, [[[Pablo]]]!': 'Hallo, Pablo!'};
+	const entry = createEntry({string: 'Hello, [[[Pablo]]]!'});
+	expect(getEntryFromTranslations(entry, translations)).toBe('Hallo, Pablo!');
+});
+
+test('get entry from missing translation', () => {
+	const translations = {};
+	const entry = createEntry({string: 'Hello, [[[Pablo]]]!'});
+	expect(getEntryFromTranslations(entry, translations)).toBe('Hello, Pablo!');
 });
