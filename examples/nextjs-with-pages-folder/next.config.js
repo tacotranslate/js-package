11fee79a56b34d275ee289f6290d80383559c8a4
Thomas Lindstrøm <t@hom.as>
2023-04-20 17:57:55 +0200

    Prevent leaking keys in example

--- examples/nextjs-with-pages-folder/next.config.js
+++ examples/nextjs-with-pages-folder/next.config.js
@@ -10,7 +10,8 @@ module.exports = async () => {
 	const isProduction =
 		process.env.TACOTRANSLATE_ENV === 'production' ||
 		process.env.VERCEL_ENV === 'production' ||
-		(!process.env.VERCEL_ENV && process.env.NODE_ENV === 'production');
+		(!(process.env.TACOTRANSLATE_ENV || process.VERCEL_ENV) &&
+			process.env.NODE_ENV === 'production');
 
 	return {
 		env: {
