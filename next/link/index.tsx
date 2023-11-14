df7a867feeade11e68173d4bea54f1fc3f49dd3b
Thomas Lindstrøm <t@hom.as>
2023-11-14 23:46:43 +0100

    Forward the reference on the link

--- next/link/index.tsx
+++ next/link/index.tsx
@@ -1,22 +1,24 @@
 'use client';
 
 import NextLink, {type LinkProps} from 'next/link';
-import React, {type ReactNode} from 'react';
+import React, {type ForwardedRef, forwardRef, type ReactNode} from 'react';
 import {useLocale} from '../../react';
 
 function getHref(href: string | LinkProps['href']) {
 	return (typeof href === 'string' ? href : href.pathname) ?? '';
 }
 
-export default function Link({
-	href,
-	...rest
-}: LinkProps & {readonly children: ReactNode}) {
+function Link(
+	{href, ...rest}: LinkProps & {readonly children: ReactNode},
+	reference: ForwardedRef<HTMLAnchorElement>
+) {
 	const locale = useLocale();
 	const patchedHref =
 		!locale || locale === process.env.TACOTRANSLATE_PROJECT_LOCALE
 			? getHref(href)
 			: `/${locale}${getHref(href)}`;
 
-	return <NextLink {...rest} href={patchedHref} />;
+	return <NextLink {...rest} ref={reference} href={patchedHref} />;
 }
+
+export default forwardRef(Link);
