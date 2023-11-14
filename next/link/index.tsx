283feb1f2542f7687d5dc39e4242120c8baff9f2
Thomas Lindstrøm <t@hom.as>
2023-11-14 23:54:15 +0100

    Fix Link component typing

--- next/link/index.tsx
+++ next/link/index.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import NextLink, {type LinkProps} from 'next/link';
-import React, {type ForwardedRef, forwardRef, type ReactNode} from 'react';
+import React, {
+	type ForwardedRef,
+	forwardRef,
+	type ComponentProps,
+} from 'react';
 import {useLocale} from '../../react';
 
 function getHref(href: string | LinkProps['href']) {
@@ -9,7 +13,7 @@ function getHref(href: string | LinkProps['href']) {
 }
 
 function Link(
-	{href, ...rest}: LinkProps & {readonly children: ReactNode},
+	{href, ...rest}: ComponentProps<typeof NextLink>,
 	reference: ForwardedRef<HTMLAnchorElement>
 ) {
 	const locale = useLocale();
