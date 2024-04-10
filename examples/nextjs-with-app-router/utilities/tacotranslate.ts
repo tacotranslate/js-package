6539fac4b37def358937831fa542395fa63bbf2d
Hamza Saleem <hamza.saleem2@yahoo.com>
2024-04-10 18:10:36 +0500

    Fix tacotranslate.ts
    reason:
    API key being potentially undefined.
    
    desc:
    Now Provided a default value of an empty string ("") for the apiKey variable if none of the environment variables are defined.

--- examples/nextjs-with-app-router/utilities/tacotranslate.ts
+++ examples/nextjs-with-app-router/utilities/tacotranslate.ts
@@ -8,7 +8,8 @@ const tacoTranslate = createTacoTranslateClient({
 	apiKey:
 		process.env.TACOTRANSLATE_SECRET_API_KEY ??
 		process.env.TACOTRANSLATE_PUBLIC_API_KEY ??
-		process.env.TACOTRANSLATE_API_KEY,
+		process.env.TACOTRANSLATE_API_KEY ??
+		"",
 	projectLocale:
 		process.env.TACOTRANSLATE_IS_PRODUCTION === 'true'
 			? defaultLocale
