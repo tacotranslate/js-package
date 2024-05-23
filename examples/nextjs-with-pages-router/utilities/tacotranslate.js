a7e433660888368f8d45f1fd07931c4369007828
Thomas Lindstrøm <t@hom.as>
2024-05-23 23:10:09 +0200

    Fallback to empty string

--- examples/nextjs-with-pages-router/utilities/tacotranslate.js
+++ examples/nextjs-with-pages-router/utilities/tacotranslate.js
@@ -4,10 +4,12 @@ const tacoTranslate = createTacoTranslateClient({
 	apiKey:
 		process.env.TACOTRANSLATE_SECRET_API_KEY ??
 		process.env.TACOTRANSLATE_PUBLIC_API_KEY ??
-		process.env.TACOTRANSLATE_API_KEY,
+		process.env.TACOTRANSLATE_API_KEY ??
+		'',
 	projectLocale:
 		process.env.TACOTRANSLATE_PROJECT_LOCALE ??
-		process.env.TACOTRANSLATE_DEFAULT_LOCALE,
+		process.env.TACOTRANSLATE_DEFAULT_LOCALE ??
+		'',
 });
 
 module.exports = tacoTranslate;
