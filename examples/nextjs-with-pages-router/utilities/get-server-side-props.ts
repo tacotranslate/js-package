a69f6fa5564f769ab4ee1cab707a9135f789bd11
Thomas Lindstrøm <t@hom.as>
2024-05-27 13:57:09 +0200

    Update `get-server-side-props.ts` code

--- examples/nextjs-with-pages-router/utilities/get-server-side-props.ts
+++ examples/nextjs-with-pages-router/utilities/get-server-side-props.ts
@@ -1,14 +1,17 @@
 import {type GetServerSidePropsContext} from 'next';
+import {type Origin} from 'tacotranslate';
 import tacoTranslate from './tacotranslate';
 
 export default async function customGetServerSideProps(
-	context: GetServerSidePropsContext
+	context: GetServerSidePropsContext,
+	additionalOrigins: Origin[] = []
 ) {
 	const {locale = process.env.TACOTRANSLATE_DEFAULT_LOCALE, locales} = context;
 	const origin = process.env.TACOTRANSLATE_ORIGIN;
-	const translations = await tacoTranslate.getTranslations({locale, origin});
+	const origins = [origin, ...additionalOrigins];
+	const localizations = await tacoTranslate.getLocalizations({locale, origins});
 
 	return {
-		props: {locale, locales, translations, origin},
+		props: {locale, locales, localizations, origin},
 	};
 }
