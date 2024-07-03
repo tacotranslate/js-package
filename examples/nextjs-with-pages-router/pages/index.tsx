21091bb5f8d4d906a2d72a7c59e1d199701f1bb9
Thomas Lindstrøm <t@hom.as>
2024-07-03 19:04:05 +0200

    Fix invalid HTML attribute quotes

--- examples/nextjs-with-pages-router/pages/index.tsx
+++ examples/nextjs-with-pages-router/pages/index.tsx
@@ -77,7 +77,7 @@ export default function Page({locales: supportedLocales}: PageProperties) {
 				}}
 			>
 				<Translate
-					string={`TacoTranslate is a service that automatically translates your React application to any language. Say [[[<span lang="es">adiós</span>]]] to JSON files, and <a href='https://tacotranslate.com'>set up your free TacoTranslate account</a> today!<br><br>Out of the box, you’ll be able to translate into <strong>one language for free</strong>.<br><a href='https://tacotranslate.com'>Sign up now, [[[<span lang="es">amigo</span>]]]!</a><br><br><strong>Oh, and did you know</strong> that if you share this page you’ll see an OpenGraph image translated into the current language, too!`}
+					string={`TacoTranslate is a service that automatically translates your React application to any language. Say [[[<span lang="es">adiós</span>]]] to JSON files, and <a href="https://tacotranslate.com">set up your free TacoTranslate account</a> today!<br><br>Out of the box, you’ll be able to translate into <strong>one language for free</strong>.<br><a href="https://tacotranslate.com">Sign up now, [[[<span lang="es">amigo</span>]]]!</a><br><br><strong>Oh, and did you know</strong> that if you share this page you’ll see an OpenGraph image translated into the current language, too!`}
 				/>
 			</p>
 		</Wrapper>
